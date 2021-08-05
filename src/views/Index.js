65d1771fe5d011d5fcd951636ace8c02c4657394
一休哥 <yixiu@yixiugedeMacBook-Pro.local>
2021-08-05 20:53:49 +0800

    修改提示语，以及按钮形状

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Space,Input, Button, Table } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
 import https from '../api/https'
 var blake2b = require('blake2b');
 const { TextArea } = Input;
@@ -32,7 +33,7 @@ export default class AddShop extends React.Component {
               key: 'action',
               render: record => (
                 <Space size="middle">
-                    <Button type="primary" size={'small'} onClick={() => this.add(record)}>抢注该账号</Button>
+                    <Button type="primary" size={'normal'} shape="round" onClick={() => this.add(record)}>抢注该账号</Button>
                 </Space>
               ),
             },
@@ -69,7 +70,7 @@ export default class AddShop extends React.Component {
                     
                 }else if(item.length>4 && item.length<10){
 
-                    if(this.strInit(item)){
+                    if(this.canRegister(item)){
                         let str = item + '.bit';
                         if(!arr.includes(str) && !reserved.includes(str) && !registered.includes(str)){
                             arr.push(item);
@@ -100,19 +101,21 @@ export default class AddShop extends React.Component {
 
         
     }
-    //校验
-    strInit = text => {
+    // 校验一个账号是否已开放注册，用于 5- 9 位账号
+    // 5-9 位，只开放 5 % 采用 blake2b 算法对账户名(包含 .bit 后缀)进行 hash ，取 hash 结果的第 1 个字节作为一个 u8 整数，当该整数小于等于 12 时，即可注册。
+    canRegister = text => {
 
         text += '.bit';
         var hash = blake2b(32, null, null, Buffer.from('2021-07-22 12:00'));
         hash = hash.update(Buffer.from(text));
         var output = new Uint8Array(32)
         var out = hash.digest(output);
-        if(out[0]<13){
+        if(out[0] < 13){
             //console.log(text,out[0])
             return true
         } 
-        else return false
+        
+        return false
     }
 
     add = record =>{
@@ -158,12 +161,12 @@ export default class AddShop extends React.Component {
                 <div style={{display:'inline-block',position:'absolute',right:15,top:18,textAlign:'right'}}>
                     <a style={{color:'#1890ff'}} href="https://da.systems/explorer?inviter=cryptofans.bit&channel=cryptofans.bit&locale=zh-CN&utm_source=cryptofans+">【了解DAS】</a>
                 </div>
-                <div>你可以将你将要的账号列表输入/粘贴到下方的编辑框内，查询哪些账号是可注册的。</div>
+                <div>将你想要的账号列表输入/粘贴到下方的编辑框内，查询哪些账号是可注册的～～。</div>
                 <br />
                 <div style={{position:'relative',paddingRight:70}}>
                     <TextArea onChange={(e)=> this.textAreaChange(e)} placeholder="" autoSize />
-                    <div style={{display:'inline-block',position:'absolute',right:0,top:0,width:70,textAlign:'right'}}>
-                        <Button type="primary" onClick={()=> this.search()}>查询</Button>
+                    <div style={{display:'inline-block',position:'absolute',right:-5,top:0,width:70,textAlign:'right'}}>
+                        <Button type="primary" shape="round" icon={<SearchOutlined />} onClick={()=> this.search()}>查询</Button>
                     </div>
                 </div>
                 <br />
